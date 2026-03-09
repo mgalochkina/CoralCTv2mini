ae3e28ef5ef7cb281840748f188cab8df86d40c6
Mariya Galochkina <70597511+mgalochkina@users.noreply.github.com>
2026-03-09 16:42:04 -0400

    Update minSig.m

--- minSig.m
+++ minSig.m
@@ -8,12 +8,3 @@ function MSE = minSig(Sigma, peaklocs, peakvals, centers,counts)
     options = statset('RobustWgtFun',[]);
     [~,~,~,~,MSE] = nlinfit(centers',counts',@(b,x)nlognfit(b,x),b0,options);
 end
-
-% function MSE = minSig(Sigmas, peaklocs, peakvals, centers, counts)
-%     b0 = [peakvals./sum(peakvals).*sum(counts)
-%           log(peaklocs) - Sigmas.^2;
-%           Sigmas];
-%     b0 = reshape(b0,[],1);
-%     options = statset('RobustWgtFun',[]);
-%     [~,~,~,~,MSE] = nlinfit(centers', counts', @(b,x)nlognfit(b,x), b0, options);
-% end
